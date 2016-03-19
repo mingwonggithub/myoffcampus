ec7940e4506d5a152aa63380c17015b7095c72c3
Shannon Hood <shood@outlook.com>
2016-03-19 17:24:16 -0400

    Basic views set up, especially worked on login/register

--- IonicMaterialDesign/www/js/shared/controllers.js
+++ IonicMaterialDesign/www/js/shared/controllers.js
@@ -32,3 +32,73 @@ appControllers.controller('toastController', function ($scope, displayOption) {
     $scope.displayOption = displayOption;
 });// End Controller for Toast.
 
+
+.controller('LoginCtrl', function($scope, $state, Orders) {
+    var self = this;
+    $scope.login = function() {
+        console.log("login executed");
+        console.log(self.email);
+        var loggedUser = Backendless.UserService.login(self.email, self.password);
+        console.log("User has been logged in: " + loggedUser);
+
+        var email = Backendless.UserService.getCurrentUser();
+        if (email != null) {
+            console.log("logedin");
+            $state.go("app.orders"); //fix this redirect
+        } else {
+            console.log("User hasn't been logged");
+        }
+    }
+})
+
+.controller('RegisterCtrl', function($scope, $state, Orders) {
+    var self = this;
+    self.email = "";
+    self.password = "";
+    self.name = "";
+
+    $scope.register = function() {
+        // body...
+        console.log("save to new page ");
+        console.log(self.email + "  " + self.name);
+
+        function userRegistered(email) {
+            Orders.gUser = email;
+            alert("email has been registered:" + email.email);
+        } -
+
+        function gotError(err) // see more on error handling
+        {
+            console.log("error message - " + err.message);
+            console.log("error code - " + err.statusCode);
+        }
+
+        var email = new Backendless.User();
+        email.email = self.email;
+        email.password = self.password;
+        email.name = self.name;
+        Backendless.UserService.register(
+            email, new Backendless.Async(
+                this.userRegistered, this.gotError));
+
+        $state.go("app.orders"); //fix this redirect
+    };
+})
+
+.controller('WelcomeCtrl', function($scope, $state, Orders) {
+    var self = this;
+    $scope.login = function() {
+        console.log("login executed");
+        console.log(self.email);
+        var loggedUser = Backendless.UserService.login(self.email, self.password);
+        console.log("User has been logged in: " + loggedUser);
+
+        var email = Backendless.UserService.getCurrentUser();
+        if (email != null) {
+            console.log("logedin");
+            $state.go("app.orders"); //fix this redirect
+        } else {
+            console.log("User hasn't been logged");
+        }
+    }
+})
